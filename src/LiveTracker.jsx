ed0f1888a7c6103c3c20e98147d92391af558eec
Sumit031202 <sumitkr.031202@gmail.com>
2026-06-11 16:36:27 +0530

    feat: display the people wating

--- src/LiveTracker.jsx
+++ src/LiveTracker.jsx
@@ -3,11 +3,16 @@ import { useState } from "react"
 
 function LiveTracker(){
     const [liveCount,setLiveCount]=useState(0);
+    const [waitingQueue,setWaitingQueue]=useState([]);
 
   useEffect(() => {
     const eventSource = new EventSource("http://localhost:8080/api/patients/stream");
     eventSource.addEventListener("Queue-Update",(e)=>{
-        setLiveCount(parseInt(e.data));
+        const queue=JSON.parse(e.data);
+        // console.log(data);
+        // console.log(data.length);
+        setLiveCount(queue.length);
+        setWaitingQueue(queue);
     })
         return () => {
             eventSource.close();
@@ -20,6 +25,11 @@ function LiveTracker(){
                 <h1>Live Queue Monitor</h1>
             </div>
             <p>Patients Currently Waiting: {liveCount}</p>
+            <ul>
+                {waitingQueue.map((patient)=>{
+                    return <li key={patient.id}>{patient.fullName}</li>
+                })}
+            </ul>
         </>
     )
 
