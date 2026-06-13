c06cdb7027cce8fdd435a918d91771c09595d74c
Sumit031202 <sumitkr.031202@gmail.com>
2026-06-14 03:22:01 +0530

    feat: add register logic and handle empty queue state

--- src/PatientDashboard.jsx
+++ src/PatientDashboard.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { act, useEffect } from "react";
 import { useState } from "react"
 
 function PatientDashboard(){
     const [liveCount,setLiveCount]=useState(0);
     const [waitingQueue,setWaitingQueue]=useState([]);
-    const [activePatient,setActivepatient]=useState();
+    const [activePatient,setActivepatient]=useState("Nobody");
+    const [patientName,setPatientName]=useState("");
 
   useEffect(() => {
     const eventSource = new EventSource("http://localhost:8080/api/patients/stream");
@@ -16,28 +17,50 @@ function PatientDashboard(){
         setWaitingQueue(queue);
     })
     eventSource.addEventListener("Active-Patient",(e)=>{
-        const patient=JSON.parse(e.data).fullName;
-        setActivepatient(patient);
+        let patientData=JSON.parse(e.data);
+        if(typeof patientData=="string"){
+            patientData=JSON.parse(patientData);
+        }
+        setActivepatient(patientData.fullName);
     })
         return () => {
             eventSource.close();
         };
     }, []);
 
+    const handleNameChange=(e)=>{
+        setPatientName(e.target.value);
+    }
+
+    const register=async()=>{
+        const response=await fetch(`http://localhost:8080/api/patients/queue/join/${patientName}`,{
+            method: "POST"
+        })
+        const data=await response.json();
+        console.log(data);
+        setPatientName("");
+    }
+
     return(
-        <>
-            <div>
-                <h1>Live Queue Monitor</h1>
+        <>  
+            <div className="Patient" style={{border:"2px solid black",margin:"5px"}}>
+                <p>Patients Currently Waiting: {liveCount}</p>
+                <div>
+                    <input onChange={handleNameChange} type="text" value={patientName} placeholder="Enter Patient Full Name..." />
+                    <button onClick={register}>Register Patient</button>
+                    <h1>Live Queue Monitor</h1>
+                </div>
+                {activePatient!=="Nobody" && activePatient.fullName!=="Nobody"?(
+                    <p><strong>{activePatient}</strong> is being served now</p>
+                ):(
+                    <p>Nobody is in the Queue</p>
+                )}
+                <ul>
+                    {waitingQueue.map((patient)=>{
+                        return <li key={patient.id}>{patient.fullName}</li>
+                    })}
+                </ul>
             </div>
-            <p>Patients Currently Waiting: {liveCount}</p>
-            <p>
-                {activePatient} is being served now (Live Tracker)
-            </p>
-            <ul>
-                {waitingQueue.map((patient)=>{
-                    return <li key={patient.id}>{patient.fullName}</li>
-                })}
-            </ul>
         </>
     )
 
