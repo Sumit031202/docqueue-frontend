6802c542fe263469f1f34cd92ec02df29ca4eb15
Sumit031202 <sumitkr.031202@gmail.com>
2026-06-15 02:10:51 +0530

    feat: restructure complete of doctor dashboard for desktop

--- src/DoctorDashboard.jsx
+++ src/DoctorDashboard.jsx
@@ -1,4 +1,7 @@
 import { act, useEffect, useState } from "react";
+import avatar from "./assets/user.svg"
+import queue from "./assets/queue.svg"
+import arrow from "./assets/arrow.svg"
 
 function DoctorDashboard(){
     // const [patientName,setPatientName]=useState("")
@@ -55,28 +58,45 @@ function DoctorDashboard(){
     }
     return(
         <>
+
             <div className="doctor-dashboard">
-                <h2>Doctor Dashboard</h2>
+                <div className="dashboard-header">
+                    <h2>Doctor Dashboard</h2>
+                    <div className="header-status">  
+                        {/* will work on this feature later, when live it should glow green else red and show live time */}
+                        {/* <span className="status-live">Live</span>  */}
+                        {/* <span className="status-time"></span> */}
+                    </div>
+                </div>
+                {/* <h2>Doctor Dashboard</h2> */}
                 <div className="current-patient-card card">
-                    <h3>Currently Serving</h3>
+                    <img className="avatar" src={avatar}></img>
                     {activePatient.fullName!=="Nobody"?
-                    <p>{activePatient.fullName} {activePatient.id}</p>:
-                    <p>No active patient</p>}
+                    <div className="active-patient-info">
+                        <h3>Currently Serving</h3>
+                        <div className="active-token">#{activePatient.id}</div>
+                        <div className="name">{activePatient.fullName}</div>
+                    </div>
+                    :<p>No active patient</p>}
                 </div>
                 <div className="next-patient-card card">
                     <button className="next-btn" onClick={callNextPatient} disabled={waitingQueue.length===0}>
-                        Next Patient
+                        <div className="aloo">Next Patient</div>
+                        <div className="pyaaz"><img src={arrow} alt="" /></div>
                     </button>
                 </div>
                 <div className="queue-card card">
-                    {waitingQueue.length===1?<h4>No Patients are in Queue</h4>:
-                    <>
-                        <h4>Live Queue ({waitingQueue.length})</h4>
+                    {waitingQueue.length===0?<h4>No Patients are in Queue</h4>:
+                    <>  
+                        <div className="queue-title">
+                            <img src={queue} alt="" />
+                            <div className="title">Live Queue ({waitingQueue.length})</div>
+                        </div>
                         <ol>
-                            <li>
+                            {/* <li>
                                 <div className="token-no">12</div>
                                 <div className="patient-name">Ravi Kumar</div> 
-                            </li>
+                            </li> */}
                             {waitingQueue.map((patient)=>{
                                 return <li key={patient.id}>
                                     <div className="token-no">{patient.id}</div>
