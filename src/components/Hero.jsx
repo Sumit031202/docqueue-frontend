fccf542283e67180d8c2df3b646526268bd28a46
Sumit031202 <sumitkr.031202@gmail.com>
2026-08-08 02:54:41 +0530

    feat: implement landing page hero section right part
    Signed-off-by: Sumit031202 <sumitkr.031202@gmail.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import "./Hero.css"
+import arrow from "../assets/arrow.svg"
 function Hero() {
     return (
         <section className="hero">
@@ -23,12 +24,12 @@ function Hero() {
                     </p>
                     <div className="hero-buttons">
                         <button className="primary-btn">
+                            <span>🔍</span>
                             Find Clinics
-                            <span>→</span>
                         </button>
                         <button className="secondary-btn">
                             Register Clinic
-                            <span>→</span>
+                            <span><img src={arrow} alt="arrow" /></span>
                         </button>
                     </div>
                     <ul className="trust-points">
@@ -47,8 +48,70 @@ function Hero() {
                     </ul>
                 </section>
                 <section className="hero-right">
-                    <div className="dashboard-preview"></div>
-                    <div className="patient-preview"></div>
+                    <div className="dashboard-preview">
+                        <div className="preview-header">
+                            <h3>Doctor Dashboard</h3>
+                            <div className="live-status">
+                                <span className="live-dot"></span>
+                                Live
+                            </div>
+                        </div>
+                        <div className="metrics">
+                            <div className="metric-card">
+                                <p>Current</p>
+                                <h2>14</h2>
+                            </div>
+                            <div className="metric-card">
+                                <p>Waiting</p>
+                                <h2>8</h2>
+                            </div>
+                            <div className="metric-card">
+                                <p>Completed</p>
+                                <h2>45</h2>
+                            </div>
+                            <div className="metric-card">
+                                <p>Avg. Wait</p>
+                                <h2>22 min</h2>
+                            </div>
+                        </div>
+                        <div className="current-queue">
+                            <h4>Current Queue</h4>
+                            <ul>
+                                <li><span>14</span>Ravi Kumar</li>
+                                <li><span>15</span>Sunita Devi</li>
+                                <li><span>16</span>Asif Ali</li>
+                                <li><span>17</span>Pooja Singh</li>
+                            </ul>
+                        </div>
+                        <div className="next">
+                            <button>Call Next Patient</button>
+                        </div>
+                    </div>
+                    <div className="patient-preview">
+                        <h3>Your Queue Status</h3>
+                        <div className="token-box">
+                            <p>Your Token</p>
+                            <h1>#14</h1>
+                        </div>
+                        <div className="patient-info">
+                            <div>
+                                <span>Patient Ahead</span>
+                                <strong>3</strong>
+                            </div>
+
+                            <div>
+                                <span>Estimated Wait</span>
+                                <strong>22 min</strong>
+                            </div>
+                            <div>
+                                <span>Clinic Open</span>
+                                <strong className="open">
+                                    <div className="live-dot"></div>
+                                    <span>Open</span>
+                                </strong>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </section>
